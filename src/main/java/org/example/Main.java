34ec20d63358d6ea166c829ab2c4de2a84e6d32b
CRTXScyNet <CRTXScyNet@gmail.com>
2024-04-21 15:16:43 +0500

    HW-5 to check

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,22 +6,22 @@ public class Main {
         System.out.println("Hello world!");
 
         // Task one
-        boolean clientOS = true;
-        if (clientOS) {
+        int clientOS = 1;
+        if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else {
+        } else if(clientOS == 0){
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
         //Task two
-        int yearOfManufacture = 2015;
-        if (clientOS) {
+        int yearOfManufacture = 2014;
+        if (clientOS == 1) {
             if (yearOfManufacture >= 2015) {
                 System.out.println("Установите версию приложения для Android по ссылке");
             } else {
                 System.out.println("Установите облегченную версию приложения для Android по ссылке");
             }
-        } else {
+        } else if(clientOS == 0) {
             if (yearOfManufacture >= 2015) {
                 System.out.println("Установите версию приложения для iOS по ссылке");
             } else {
@@ -30,7 +30,6 @@ public class Main {
         }
 
         //Task three
-
         int year = 2000;
         if (year <= 1584) {
             System.out.println(year + " год не является високосным");
@@ -59,34 +58,24 @@ public class Main {
         }
 
         // Task five
-        int monthNumber = 12;
+        int monthNumber = 5;
         if (monthNumber <= 12) {
             switch (monthNumber) {
-                case 1:
-                case 2:
+                case 1, 2, 12:
                     System.out.println(monthNumber + " месяц принадлежит зиме.");
                     break;
-                case 3:
-                case 4:
-                case 5:
+                case 3, 4, 5:
                     System.out.println(monthNumber + " месяц принадлежит весне.");
                     break;
-                case 6:
-                case 7:
-                case 8:
+                case 6, 7, 8:
                     System.out.println(monthNumber + " месяц принадлежит лету.");
                     break;
-                case 9:
-                case 10:
-                case 11:
+                case 9, 10, 11:
                     System.out.println(monthNumber + " месяц принадлежит осени.");
                     break;
-                case 12:
-                    System.out.println(monthNumber + " месяц принадлежит зиме.");
-                    break;
-                default:
-                    System.out.println("Такого месяца не существует");
             }
+        } else {
+            System.out.println("Такого месяца не существует");
         }
     }
 }
